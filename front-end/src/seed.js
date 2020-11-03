1e5be7797735321ba1847b880bf8df99dcb6f7d7
Selenophilia <pauloargenal@gmail.com>
2020-11-03 20:13:36 +0800

    Added new fields for product details

--- front-end/src/seed.js
+++ front-end/src/seed.js
@@ -9,7 +9,8 @@ const data = {
             image: '/images/ps4-controller.jpeg',
             brand: 'Sony',
             numReviews: 10,
-            description: 'a sony made playstation console'
+            description: 'a sony made playstation console',
+            numStock: 20
         },
         {
             id: 2,
@@ -20,7 +21,8 @@ const data = {
             image: '/images/xbox-ctrl.jpeg',
             brand: 'Microsoft',
             numReviews: 20,
-            description: 'a high quality controller'
+            description: 'a high quality controller',
+            numStock: 0
         },
         {
             id: 3,
@@ -31,7 +33,8 @@ const data = {
             image: '/images/mouse-LG.jpeg',
             brand: 'logitech',
             numReviews: 30,
-            description: 'a high quality mouse made by logitech'
+            description: 'a high quality mouse made by logitech',
+            numStock: 30
         },
         {
             id: 4,
@@ -42,7 +45,8 @@ const data = {
             image: '/images/mouse-Razer.jpeg',
             brand: 'Razer',
             numReviews: 30,
-            description: 'a high quality mouse made by Razer'
+            description: 'a high quality mouse made by Razer',
+            numStock: 40
         },
         {
             id: 5,
@@ -53,7 +57,8 @@ const data = {
             image: '/images/keeb-1.jpeg',
             brand: 'keeb-fam',
             numReviews: 40,
-            description: 'a high quality mechanical keyboard'
+            description: 'a high quality mechanical keyboard',
+            numStock: 50
         },
         {
             id: 6,
@@ -64,7 +69,8 @@ const data = {
             image: '/images/keeb-2.jpeg',
             brand: 'keeb-fam',
             numReviews: 40,
-            description: 'a high quality mechanical keyboard'
+            description: 'a high quality mechanical keyboard',
+            numStock: 10
         },
         {
             id: 7,
@@ -75,7 +81,8 @@ const data = {
             image: '/images/keeb-3.jpeg',
             brand: 'keeb-fam',
             numReviews: 30,
-            description: 'a high quality mechanical keyboard'
+            description: 'a high quality mechanical keyboard',
+            numStock: 5
         },
     ]
 }
