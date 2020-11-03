58085b5f8bc538f56dba21c59338aa9bd4dc620a
Selenophilia <pauloargenal@gmail.com>
2020-11-03 16:13:31 +0800

    Modified initial data

--- front-end/src/seed.js
+++ front-end/src/seed.js
@@ -14,30 +14,30 @@ const data = {
             id: 2,
             name: "Xbox 360 controller",
             price: 120,
-            rating: 5,
+            rating: 4.5,
             image: '/images/xbox-ctrl.jpeg',
             brand: 'Microsoft',
-            numReviews: 10,
+            numReviews: 20,
             description: 'a high quality controller'
         },
         {
             id: 3,
             name: "logitech-502",
             price: 300,
-            rating: 5,
+            rating: 4.9,
             image: '/images/mouse-LG.jpeg',
             brand: 'logitech',
-            numReviews: 10,
+            numReviews: 30,
             description: 'a high quality mouse made by logitech'
         },
         {
             id: 4,
             name: "Razer Mouse",
             price: 300,
-            rating: 5,
+            rating: 4,
             image: '/images/mouse-Razer.jpeg',
             brand: 'Razer',
-            numReviews: 10,
+            numReviews: 30,
             description: 'a high quality mouse made by Razer'
         },
         {
@@ -47,17 +47,17 @@ const data = {
             rating: 5,
             image: '/images/keeb-1.jpeg',
             brand: 'keeb-fam',
-            numReviews: 10,
+            numReviews: 40,
             description: 'a high quality mechanical keyboard'
         },
         {
             id: 6,
             name: "mech-keyboard-2",
             price: 300,
-            rating: 5,
+            rating: 4.3,
             image: '/images/keeb-2.jpeg',
             brand: 'keeb-fam',
-            numReviews: 10,
+            numReviews: 40,
             description: 'a high quality mechanical keyboard'
         },
         {
@@ -67,7 +67,7 @@ const data = {
             rating: 5,
             image: '/images/keeb-3.jpeg',
             brand: 'keeb-fam',
-            numReviews: 10,
+            numReviews: 30,
             description: 'a high quality mechanical keyboard'
         },
     ]
