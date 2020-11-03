5b51b4c66dad0ff44411777f90c7df2cfb6bfb2a
Selenophilia <pauloargenal@gmail.com>
2020-11-03 15:52:19 +0800

    Integrated the styles to the react-app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,177 @@
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div class="container">
+       <header class="row">
+                <div>
+                    <a href="#"> XenElectronic </a>
+                </div>
+                
+                <div>
+                    <a href='#'> <span class="icon"><i class="fa fa-shopping-cart"></i></span></a>
+                    <a href="#">Sign In </a>  
+                </div>
+             
+        </header>
+    
+        <section class="products">
+
+          <div class="row center">
+                <div class="card">
+                    <a href="#">
+                      <img class="medium" src="./images/ps4-controller.jpeg" alt='product'/>   
+                      </a>
+                      <div class="card-body">
+                          <div class="product-details">
+                              <a href="#">
+                                  <h1> Play Station 4</h1>    
+                              </a>
+                              <span class='price'>
+                                  <h2> Price: $200.00</h2>
+                              </span>
+                              
+                          </div>
+                          <div class="product-rating">
+                              <span><i class="fa fa-star"></i> </span>
+                              <span><i class="fa fa-star"></i> </span>
+                              <span><i class="fa fa-star"></i> </span>
+                              <span><i class="fa fa-star"></i> </span>
+                              <span><i class="fa fa-star"></i> </span>             
+                          </div>           
+                      </div>                        
+                  </div>  
+                  
+                  <div class="card">
+                        <a href="#">
+                         <img class="medium" src="./images/xbox-ctrl.jpeg" alt='product' />   
+                         </a>
+                         <div class="card-body">
+                             <div class="product-details">
+                                 <a href="#">
+                                     <h1> XBox 360 controller</h1>    
+                                 </a>
+                                 <span class='price'>
+                                     <h2> Price: $200.00</h2>
+                                 </span>
+                                 
+                             </div>
+                             <div class="product-rating">
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>             
+                             </div>           
+                         </div>                        
+                     </div>   
+
+
+                     <div class="card">
+                        <a href="#">
+                         <img class="medium" src="./images/keeb-1.jpeg" alt='product' />   
+                         </a>
+                         <div class="card-body">
+                             <div class="product-details">
+                                 <a href="#">
+                                     <h1> Mechanical Keyboard</h1>    
+                                 </a>
+                                 <span class='price'>
+                                     <h2> Price: $200.00</h2>
+                                 </span>
+                                 
+                             </div>
+                             <div class="product-rating">
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>             
+                             </div>           
+                         </div>                        
+                    </div> 
+
+                    <div class="card">
+                        <a href="#">
+                         <img class="medium" src="./images/mouse-LG.jpeg" alt='product' />   
+                         </a>
+                         <div class="card-body">
+                             <div class="product-details">
+                                 <a href="#">
+                                     <h1> LG 502</h1>    
+                                 </a>
+                                 <span class='price'>
+                                     <h2> Price: $200.00</h2>
+                                 </span>
+                                 
+                             </div>
+                             <div class="product-rating">
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>             
+                             </div>           
+                         </div>                        
+                     </div>  
+
+
+                    <div class="card">
+                        <a href="#">
+                        <img class="medium" src="./images/keeb-2.jpeg" alt='product' /> 
+                         </a>
+                         <div class="card-body">
+                             <div class="product-details">
+                                 <a href="#">
+                                     <h1> Mechanical Keyboard</h1>    
+                                 </a>
+                                 <span class='price'>
+                                     <h2> Price: $200.00</h2>
+                                 </span>
+                                 
+                             </div>
+                             <div class="product-rating">
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>             
+                             </div>           
+                         </div>                        
+                     </div> 
+
+                     <div class="card">
+                        <a href="#">
+                         <img class="medium" src="./images/mouse-Razer.jpeg" alt='product' />   
+                         </a>
+                         <div class="card-body">
+                             <div class="product-details">
+                                 <a href="#">
+                                     <h1> Play Station 4</h1>    
+                                 </a>
+                                 <span class='price'>
+                                     <h2> Price: $200.00</h2>
+                                 </span>
+                                 
+                             </div>
+                             <div class="product-rating">
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>
+                                 <span><i class="fa fa-star"></i> </span>             
+                             </div>           
+                         </div>                        
+                     </div> 
+          </div>              
+        </section>
+        <footer class="row center">
+            <span> &copy 2020 All rights reserved </span>
+            <span> images from: <a href="https://unsplash.com/"> Unsplash</a> </span>
+        </footer>
     </div>
+    
   );
 }
 
