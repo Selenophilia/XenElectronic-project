aa8fbc5bb1998261d20c6eab3b8f994b70e292d7
Selenophilia <pauloargenal@gmail.com>
2020-11-03 16:11:31 +0800

    Added a seed file for initial data

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,172 +1,171 @@
-
+import './seed'
 import './App.css';
 
 function App() {
   return (
-    <div class="container">
-       <header class="row">
+    <div className="container">
+       <header className="row">
                 <div>
                     <a href="#"> XenElectronic </a>
                 </div>
                 
                 <div>
-                    <a href='#'> <span class="icon"><i class="fa fa-shopping-cart"></i></span></a>
+                    <a href='#'> <span className="icon"><i className="fa fa-shopping-cart"></i></span></a>
                     <a href="#">Sign In </a>  
                 </div>
              
         </header>
     
-        <section class="products">
-
-          <div class="row center">
-                <div class="card">
+        <section className="products">
+          <div className="row center">
+                <div className="card">
                     <a href="#">
-                      <img class="medium" src="./images/ps4-controller.jpeg" alt='product'/>   
+                      <img className="medium" src="./images/ps4-controller.jpeg" alt='product'/>   
                       </a>
-                      <div class="card-body">
-                          <div class="product-details">
+                      <div className="card-body">
+                          <div className="product-details">
                               <a href="#">
                                   <h1> Play Station 4</h1>    
                               </a>
-                              <span class='price'>
+                              <span className='price'>
                                   <h2> Price: $200.00</h2>
                               </span>
                               
                           </div>
-                          <div class="product-rating">
-                              <span><i class="fa fa-star"></i> </span>
-                              <span><i class="fa fa-star"></i> </span>
-                              <span><i class="fa fa-star"></i> </span>
-                              <span><i class="fa fa-star"></i> </span>
-                              <span><i class="fa fa-star"></i> </span>             
+                          <div className="product-rating">
+                              <span><i className="fa fa-star"></i> </span>
+                              <span><i className="fa fa-star"></i> </span>
+                              <span><i className="fa fa-star"></i> </span>
+                              <span><i className="fa fa-star"></i> </span>
+                              <span><i className="fa fa-star"></i> </span>             
                           </div>           
                       </div>                        
                   </div>  
                   
-                  <div class="card">
+                  <div className="card">
                         <a href="#">
-                         <img class="medium" src="./images/xbox-ctrl.jpeg" alt='product' />   
+                         <img className="medium" src="./images/xbox-ctrl.jpeg" alt='product' />   
                          </a>
-                         <div class="card-body">
-                             <div class="product-details">
+                         <div className="card-body">
+                             <div className="product-details">
                                  <a href="#">
                                      <h1> XBox 360 controller</h1>    
                                  </a>
-                                 <span class='price'>
+                                 <span className='price'>
                                      <h2> Price: $200.00</h2>
                                  </span>
                                  
                              </div>
-                             <div class="product-rating">
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>             
+                             <div className="product-rating">
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>             
                              </div>           
                          </div>                        
                      </div>   
 
 
-                     <div class="card">
+                     <div className="card">
                         <a href="#">
-                         <img class="medium" src="./images/keeb-1.jpeg" alt='product' />   
+                         <img className="medium" src="./images/keeb-1.jpeg" alt='product' />   
                          </a>
-                         <div class="card-body">
-                             <div class="product-details">
+                         <div className="card-body">
+                             <div className="product-details">
                                  <a href="#">
                                      <h1> Mechanical Keyboard</h1>    
                                  </a>
-                                 <span class='price'>
+                                 <span className='price'>
                                      <h2> Price: $200.00</h2>
                                  </span>
                                  
                              </div>
-                             <div class="product-rating">
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>             
+                             <div className="product-rating">
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>             
                              </div>           
                          </div>                        
                     </div> 
 
-                    <div class="card">
+                    <div className="card">
                         <a href="#">
-                         <img class="medium" src="./images/mouse-LG.jpeg" alt='product' />   
+                         <img className="medium" src="./images/mouse-LG.jpeg" alt='product' />   
                          </a>
-                         <div class="card-body">
-                             <div class="product-details">
+                         <div className="card-body">
+                             <div className="product-details">
                                  <a href="#">
                                      <h1> LG 502</h1>    
                                  </a>
-                                 <span class='price'>
+                                 <span className='price'>
                                      <h2> Price: $200.00</h2>
                                  </span>
                                  
                              </div>
-                             <div class="product-rating">
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>             
+                             <div className="product-rating">
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>             
                              </div>           
                          </div>                        
                      </div>  
 
 
-                    <div class="card">
+                    <div className="card">
                         <a href="#">
-                        <img class="medium" src="./images/keeb-2.jpeg" alt='product' /> 
+                        <img className="medium" src="./images/keeb-2.jpeg" alt='product' /> 
                          </a>
-                         <div class="card-body">
-                             <div class="product-details">
+                         <div className="card-body">
+                             <div className="product-details">
                                  <a href="#">
                                      <h1> Mechanical Keyboard</h1>    
                                  </a>
-                                 <span class='price'>
+                                 <span className='price'>
                                      <h2> Price: $200.00</h2>
                                  </span>
                                  
                              </div>
-                             <div class="product-rating">
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>             
+                             <div className="product-rating">
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>             
                              </div>           
                          </div>                        
                      </div> 
 
-                     <div class="card">
+                     <div className="card">
                         <a href="#">
-                         <img class="medium" src="./images/mouse-Razer.jpeg" alt='product' />   
+                         <img className="medium" src="./images/mouse-Razer.jpeg" alt='product' />   
                          </a>
-                         <div class="card-body">
-                             <div class="product-details">
+                         <div className="card-body">
+                             <div className="product-details">
                                  <a href="#">
                                      <h1> Play Station 4</h1>    
                                  </a>
-                                 <span class='price'>
+                                 <span className='price'>
                                      <h2> Price: $200.00</h2>
                                  </span>
                                  
                              </div>
-                             <div class="product-rating">
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>
-                                 <span><i class="fa fa-star"></i> </span>             
+                             <div className="product-rating">
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>
+                                 <span><i className="fa fa-star"></i> </span>             
                              </div>           
                          </div>                        
                      </div> 
           </div>              
         </section>
-        <footer class="row center">
+        <footer className="row center">
             <span> &copy 2020 All rights reserved </span>
             <span> images from: <a href="https://unsplash.com/"> Unsplash</a> </span>
         </footer>
---
+++ front-end/src/seed.js
@@ -0,0 +1,76 @@
+const data = {
+    products: [
+        {
+            id: 1,
+            name: "Playstation 4 Console",
+            price: 200,
+            rating: 5,
+            image: '/images/ps4-controller.jpeg',
+            brand: 'Sony',
+            numReviews: 10,
+            description: 'a sony made playstation console'
+        },
+        {
+            id: 2,
+            name: "Xbox 360 controller",
+            price: 120,
+            rating: 5,
+            image: '/images/xbox-ctrl.jpeg',
+            brand: 'Microsoft',
+            numReviews: 10,
+            description: 'a high quality controller'
+        },
+        {
+            id: 3,
+            name: "logitech-502",
+            price: 300,
+            rating: 5,
+            image: '/images/mouse-LG.jpeg',
+            brand: 'logitech',
+            numReviews: 10,
+            description: 'a high quality mouse made by logitech'
+        },
+        {
+            id: 4,
+            name: "Razer Mouse",
+            price: 300,
+            rating: 5,
+            image: '/images/mouse-Razer.jpeg',
+            brand: 'Razer',
+            numReviews: 10,
+            description: 'a high quality mouse made by Razer'
+        },
+        {
+            id: 5,
+            name: "mech-keyboard-1",
+            price: 300,
+            rating: 5,
+            image: '/images/keeb-1.jpeg',
+            brand: 'keeb-fam',
+            numReviews: 10,
+            description: 'a high quality mechanical keyboard'
+        },
+        {
+            id: 6,
+            name: "mech-keyboard-2",
+            price: 300,
+            rating: 5,
+            image: '/images/keeb-2.jpeg',
+            brand: 'keeb-fam',
+            numReviews: 10,
+            description: 'a high quality mechanical keyboard'
+        },
+        {
+            id: 7,
+            name: "mech-keyboard-3",
+            price: 300,
+            rating: 5,
+            image: '/images/keeb-3.jpeg',
+            brand: 'keeb-fam',
+            numReviews: 10,
+            description: 'a high quality mechanical keyboard'
+        },
+    ]
+}
+
+export default data
