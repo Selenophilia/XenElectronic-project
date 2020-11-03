f194765712d8a2c6d3af4c7cb8ac4b82a1c87752
Selenophilia <pauloargenal@gmail.com>
2020-11-03 23:03:32 +0800

    Added start script on package.json/modified eslintsrc file and server.js file

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,12 @@
 import express from "express";
 import data from './seed'
+
+// run node -r esm back-end/server.js to start server
+// for nodemon nodemon -r esm server.js
+
 const app = express();
+
+const  PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('Server Ready!');
@@ -10,9 +16,7 @@ app.get('/api/products', (req, res) => {
    res.send(data.products)
 });
 
-app.listen(5000, function () {
-  console.log('Listening on port 5000!');
+app.listen(PORT, function () {
+  console.log(`listening on ${PORT}`);
 });
 
-// run node -r esm back-end/server.js to start server
-// for nodemon nodemon -r esm server.js
