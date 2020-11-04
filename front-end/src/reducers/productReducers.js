e376b32f846110b8c1a89b922a5a1e5c3b1e4ffc
Selenophilia <pauloargenal@gmail.com>
2020-11-04 13:20:03 +0800

    Added redux for a single product display

--- front-end/src/reducers/productReducers.js
+++ front-end/src/reducers/productReducers.js
@@ -1,6 +1,6 @@
-const { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL } = require("../constants/productConstants");
+const { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST } = require("../constants/productConstants");
 
-export const productListReducer = (state = { products: [] }, action) => {
+export const productListReducer = (state = { products: [], loading: true }, action) => {
     switch (action.type) {
         case PRODUCT_LIST_REQUEST:
             return {loading: true};
@@ -12,3 +12,17 @@ export const productListReducer = (state = { products: [] }, action) => {
            return state;
     }
 }
+
+
+export const productDetailsReducer = (state = {product: [], loading:true}, action) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return {loading: true};
+        case PRODUCT_LIST_SUCCESS:
+            return{loading: false, product: action.payload};
+        case PRODUCT_LIST_FAIL: 
+            return{loading: false, error: action.payload};
+        default:
+           return state;
+    }
+} 
--- front-end/src/screens/ProductDetails.js
+++ front-end/src/screens/ProductDetails.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 import Rating from '../components/Rating';
-import data from '../seed'
-
+// import data from '../seed'
+import Loader from '../components/Loader'
+import AlertMessages from '../components/AlertMessages'
 
 function ProductDetails(props){
-    const product =  data.products.find((prod ) =>  prod.id ===  parseInt(props.match.params.id))
-  
+    // const product =  data.products.find((prod ) =>  prod.id ===  parseInt(props.match.params.id))
+   const productDetails = useSelector(state => state.productDetails)
+   const {error, loading, product} = productDetails
     if(!product){
         return(   
         <div className='row product-details'>
@@ -16,50 +19,54 @@ function ProductDetails(props){
         );
     }
     return(
-        <div className='row product-details'>
-                        <div className='col-2'>
-                            <img className='image__large' src={product.image} alt={props.name}/>  
-                            <p> More Images: </p> 
-                            <img className='image__small' src={product.image} alt={props.name}/>  
+        <div >
+               {loading ? ( <Loader />  
+         ) : error ?  (<AlertMessages>{error} </AlertMessages>
+         ) : (  
+         <div className='row product-details'>
+            <div className='col-2'>
+                    <img className='image__large' src={product.image} alt={props.name}/>  
+                    <p> More Images: </p> 
+                    <img className='image__small' src={product.image} alt={props.name}/>  
 
-                        </div>
+                </div>
 
-                        <div className='col-1'>
-                                    <ul>
-                                        <li>  Product Name: {product.name}</li>
-                                        <li> Price: ${product.price}</li>
-                                        <li>  <Rating reviews={product.numReviews}
-                                                      rating={product.rating} /></li>
-                                        <li>  <p> Description: </p>
-                                              <p>{product.description}</p></li>
-                                    </ul>
-                                <div className='card card-body'>
-                                    <ul>
-                                        <li>
-                                            <div className='row'>
-                                                  <div> Price: </div>  
-                                                  <div className='price'> ${product.price}</div>  
-                                            </div>
-                                            <div className='row'>
-                                                  <div> Qty: </div>  
-                                                  <div className='price'> 1</div>  
-                                            </div>
-                                            <div className='row'>
-                                                 <div> Stock:</div>  
-                                                  <div className='stock'> {product.numStock >  0 ? product.numStock : 0} in stock</div>  
-                                            </div>
-                                        </li>
-                                     
-                                        <li>
-                                           <button className='primary'> Add to Cart</button>
-                                        </li>
-                                    </ul>
-                              </div>
+                <div className='col-1'>
+                            <ul>
+                                <li>  Product Name: {product.name}</li>
+                                <li> Price: ${product.price}</li>
+                                <li>  <Rating reviews={product.numReviews}
+                                            rating={product.rating} /></li>
+                                <li>  <p> Description: </p>
+                                    <p>{product.description}</p></li>
+                            </ul>
+                        <div className='card card-body'>
+                            <ul>
+                                <li>
+                                    <div className='row'>
+                                        <div> Price: </div>  
+                                        <div className='price'> ${product.price}</div>  
+                                    </div>
+                                    <div className='row'>
+                                        <div> Qty: </div>  
+                                        <div className='price'> 1</div>  
+                                    </div>
+                                    <div className='row'>
+                                        <div> Stock:</div>  
+                                        <div className='stock'> {product.numStock >  0 ? product.numStock : 0} in stock</div>  
+                                    </div>
+                                </li>
+                            
+                                <li>
+                                    <button className='primary'> Add to Cart</button>
+                                </li>
+                            </ul>
+                    </div>
 
-                        </div>
-                   
-             </div>
-    
+                </div>    
+         </div>)}
+
+            </div>
     
     );
 }
