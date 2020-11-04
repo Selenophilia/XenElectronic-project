fa3dac5a87629bebd211b1e26f8b666add899223
Selenophilia <pauloargenal@gmail.com>
2020-11-04 12:55:24 +0800

    incorporated redux in home component

--- front-end/src/screens/Home.js
+++ front-end/src/screens/Home.js
@@ -1,49 +1,40 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect,  } from 'react'
 import Product from '../components/Product'
-import axios from 'axios'
+
 import Loader from '../components/Loader'
 import AlertMessages from '../components/AlertMessages'
-function ProductDetails(props){
-    const [products, setProduct] = useState([])
-    const [loader, setLoader] = useState(false)
-    const [error, setError] = useState(false)
+import { useDispatch, useSelector } from 'react-redux'
+import { listProducts } from '../actions/productActions'
+function ProductDetails(){
+   
     
+    const productList = useSelector((state) => state.productList);
+  
+  
+    const {loading, error, products} = productList;
+    const dispatch = useDispatch();
+  
+   
     useEffect(() => {
-       
-        const fetchProduct = async () => {
-            try{
-                setLoader(true)
-                const { data } = await axios.get('/api/products')    
-                setLoader(false)
-                setProduct(data)
-            } catch(err){
-                console.log(err.message)
-                setError(err.message)
-                setLoader(false)
-            }
-
-        }
-        fetchProduct()
-    }, [])
-
-    let getProducts = products.map((product) => 
-                    <Product key={product.id} 
-                             id={product.id}    
-                             name={product.name}
-                             price={product.price}
-                             image_url={product.image}
-                             description={product.description}
-                             brand={product.brand} 
-                             reviews={product.numReviews}
-                             rating={product.rating}/>
-    );
-
+        dispatch(listProducts());
+    }, [dispatch]);
+  
     return(
         <div className="row center">   
 
-        {loader ? ( <Loader />  
+        {loading ? ( <Loader />  
          ) : error ?  (<AlertMessages>{error} </AlertMessages>
-         ) : getProducts}
+         ) : products.map((product) => 
+                        <Product key={product.id} 
+                                id={product.id}    
+                                name={product.name}
+                                price={product.price}
+                                image_url={product.image}
+                                description={product.description}
+                                brand={product.brand} 
+                                reviews={product.numReviews}
+                                rating={product.rating}/>
+                )}
               
         </div>         
     );
